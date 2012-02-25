71f8f757d5055c69852e05865ed5a47d44e7dbc0
Mikhail Polykovskij <glader@yandex-team.ru>
2012-02-25 17:35:47 +0600

    Setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='django_subscribe', 
+    description='Simple subscribe application. Not for wide use.',
+    packages=[
+        'django_subscribe',
+    ],
+)
