d883aed7389cf7b7e4e3c0489ee8117edac274e1
Mikhail Polykovskij <glader@yandex-team.ru>
2012-07-08 21:52:25 +0600

    Fixed initial migration

--- django_subscribe/migrations/0001_initial.py
+++ django_subscribe/migrations/0001_initial.py
@@ -1,4 +1,4 @@
-# encoding: utf-8
+# -*- coding: utf-8 -*-
 import datetime
 from south.db import db
 from south.v2 import SchemaMigration
@@ -8,24 +8,24 @@ from django.db import models
 class Migration(SchemaMigration):
 
     def forwards(self, orm):
-
         # Adding model 'Subscription'
-        db.create_table('subscribe_subscription', (
+        db.create_table('django_subscribe_subscription', (
             ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('email', self.gf('django.db.models.fields.EmailField')(max_length=75)),
             ('confirmation_code', self.gf('django.db.models.fields.CharField')(default=None, max_length=32, null=True, blank=True)),
             ('delete_code', self.gf('django.db.models.fields.CharField')(default=None, max_length=32, null=True, blank=True)),
             ('date_created', self.gf('django.db.models.fields.DateField')(auto_now_add=True, blank=True)),
         ))
-        db.send_create_signal('subscribe', ['Subscription'])
+        db.send_create_signal('django_subscribe', ['Subscription'])
+
 
     def backwards(self, orm):
-
         # Deleting model 'Subscription'
-        db.delete_table('subscribe_subscription')
+        db.delete_table('django_subscribe_subscription')
+
 
     models = {
-        'subscribe.subscription': {
+        'django_subscribe.subscription': {
             'Meta': {'object_name': 'Subscription'},
             'confirmation_code': ('django.db.models.fields.CharField', [], {'default': 'None', 'max_length': '32', 'null': 'True', 'blank': 'True'}),
             'date_created': ('django.db.models.fields.DateField', [], {'auto_now_add': 'True', 'blank': 'True'}),
@@ -35,4 +35,4 @@ class Migration(SchemaMigration):
         }
     }
 
-    complete_apps = ['subscribe']
+    complete_apps = ['django_subscribe']
